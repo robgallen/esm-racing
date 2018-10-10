837aaa4d5ea085b17486e2fcb14a94eaac85a19e
Rob Allen <robgallen@gmail.com>
2018-10-10 16:09:22 +0100

    Add starter project

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ESM.Racing.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
